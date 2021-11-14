a2f407d41f82de06ac871d47801834a497c9f4ae
Firdaus Wahyu Gama Ashaari <90119969+Firdagama18@users.noreply.github.com>
2021-11-13 20:45:44 -0800

    Database pegawai

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,9 @@ Route::get('formets','ViewController@showFormets');
 
 Route::get('show','ViewController@showForm');
 Route::post('result','ViewController@result');
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
